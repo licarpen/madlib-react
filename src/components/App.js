cf85228019f066daf0835718b587bdefada17be9
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-01-29 13:24:48 -0800

    add working footer component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Madlib from '../containers/Madlib';
+import Footer from '../components/footer/Footer';
 
 export default function App() {
-  return <Madlib />;
+  return (
+    <>
+      <Madlib />
+      <Footer />
+    </>
+  );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import styles from './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        MadLibs ©
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
